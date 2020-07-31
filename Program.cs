284644fa30cbf67b12d6b48e778fa1d90f6615a9
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-07-31 16:23:47 +0930

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Dynamic;
+
+namespace ATMRefillingManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Main Menu\n");
+                Console.WriteLine("1. Add Refilling Details\n");
+                Console.WriteLine("2. Show Refilling Report\n");
+                Console.WriteLine("3. Exit\n");
+
+                Console.WriteLine("Please choose option [1,2,3]:");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Hi");
+                        break;
+                    case 2:
+                        Console.WriteLine("Hello");
+                        break;
+                    case 3: Environment.Exit(0); break;
+                    default:
+                        Console.WriteLine("PLease Enter a valid Input\n");
+                        break;
+                }
+            }
+        }
+    }
+}
