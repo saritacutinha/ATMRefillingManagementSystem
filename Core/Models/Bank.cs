696b33b2f11072c08ceb5889e8a6ee0491b18751
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-07-31 17:12:31 +0930

    Adding Persistence and Bank Model

---
+++ Core/Models/Bank.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATMRefillingManagementSystem.Core.Models
+{
+    public class Bank
+    {
+        public int BankId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Persistence/ATMRefillingManagementSystemDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ATMRefillingManagementSystem.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATMRefillingManagementSystem.Data
+{
+    public class ATMRefillingManagementSystemDbContext : DbContext
+    {
+        public DbSet<Bank> Banks { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ATMRefillingManagementSystem;Integrated Security=SSPI;MultipleActiveResultSets=true;");
+        }
+    }
+}
